30dbfd4e85639f419dc8ea5d3664b1f64ad69dfc
asl1n <122009391+asl1n@users.noreply.github.com>
2025-01-21 14:22:28 +0545

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Components/AuthForm.tsx'
+import AuthForm from './Components/AuthForm.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthForm />
   </StrictMode>,
 )
